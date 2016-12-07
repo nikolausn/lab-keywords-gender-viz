dcbbe75a9f2865ac368ce978b87350297598f15e
nikolausn <nikolaus.nova@gmail.com>
2016-12-07 14:15:31 -0600

    anon for viz

--- frontend/src/app/examples/genderviz/graph.js
+++ frontend/src/app/examples/genderviz/graph.js
@@ -7,36 +7,37 @@
  * All of these are wrapped to 'frontend.examples.book' angular module.
  */
 (function() {
-  'use strict';
+    'use strict';
 
-  // Define frontend.examples.book angular module
-  angular.module('frontend.dataviz.genderviz', ['chart.js']);
+    // Define frontend.examples.book angular module
+    angular.module('frontend.dataviz.genderviz', ['chart.js']);
 
-  // Module configuration
-  angular.module('frontend.dataviz.genderviz')
-    .config([
-      '$stateProvider',
-      function config($stateProvider) {
-        $stateProvider
-          // Book list
-          .state('examples.genderviz', {
-            url: '/dataviz/genderviz',
-            views: {
-              'content@': {
-                templateUrl: '/frontend/examples/genderviz/graph.html',
-                controller: 'GraphController',
-                resolve: {                  
-                  _keywords: [                    
-                    function resolve(GraphModel) {
-                      return '';
+    // Module configuration
+    angular.module('frontend.dataviz.genderviz')
+        .config([
+            '$stateProvider',
+            function config($stateProvider) {
+                $stateProvider
+                // Book list
+                    .state('examples.genderviz', {
+                    url: '/dataviz/genderviz',
+                    data: {
+                        access: 0
+                    },
+                    views: {
+                        'content@': {
+                            templateUrl: '/frontend/examples/genderviz/graph.html',
+                            controller: 'GraphController',
+                            resolve: {
+                                _keywords: [
+                                    function resolve(GraphModel) {
+                                        return '';
+                                    }
+                                ]
+                            }
+                        }
                     }
-                  ]
-                }
-              }
+                });
             }
-          })
-          ;          
-      }
-    ])
-  ;
+        ]);
 }());
